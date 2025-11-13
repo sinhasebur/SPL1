b05f1de32c6164e9a27bd2d0f171ae889f4d5dea
Hasebur Sinha <sinhasebur932@gmail.com>
2025-11-13 19:05:18 +0600

    start

--- PracticeCodes/start.c
+++ PracticeCodes/start.c
@@ -1,1 +1,42 @@
-,,
+#include<stdio.h>
+#include<string.h>
+void swapBits(char* string);
+void swap(char* a, char* b);
+
+int main(){
+    char plainText[9]; 
+    scanf("%8s", plainText);
+    
+    printf("64 bits are: \n");
+
+    for(int i = 0;i<8;i++) {
+        printf("%02X ", plainText[i]);
+    }
+    swapBits(plainText);
+    
+    printf("\nswapped bits are: \n");
+    for(int i = 0;i<8;i++) {
+        printf("%02X ", plainText[i]);
+    }
+    
+    swapBits(plainText);
+    printf("\nswapped bits are: \n");
+    for(int i = 0;i<8;i++) {
+        printf("%02X ", plainText[i]);
+    }
+    printf("\n");
+
+    
+}
+
+
+void swapBits(char* temp){
+    for(int i=0;i<4;i++){
+        swap(&temp[i],&temp[4+i]);
+    }
+}
+
+void swap(char* a, char* b){
+    char temp;
+    temp=*a; *a=*b; *b=temp;
+}
