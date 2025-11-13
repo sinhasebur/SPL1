b30a82db8fd540448fda36e3ea3faffe2e3ad621
Hasebur Sinha <sinhasebur932@gmail.com>
2025-11-14 03:27:41 +0600

    sboxes

--- PracticeCodes/start.c
+++ PracticeCodes/start.c
@@ -2,11 +2,15 @@
 #include<string.h>
 #include <stdbool.h>
 
+#define endl printf("\n");
+
 void swapBits(char* string);
 void swap(char* a, char* b);
 void mangler(char* temp);
-void fillExpansionMatrix(char* temp);
+void fillExpansionMatrix(char* temp, int* x);
 void charToBinary(int c, int*x);
+void sbox(int* x, int *y);
+int binToInt(int x);
 
 int main()
 {
@@ -25,12 +29,6 @@ int main()
         printf("%02X ", plainText[i]);
     }
     
-    // swapBits(plainText);
-    // printf("\nswapped bits are: \n");
-    // for(int i = 0;i<8;i++) {
-    //     printf("%02X ", plainText[i]);
-    // }
-    // printf("\n");
 
     char leftHalf[4]={plainText[0],plainText[1],plainText[2],plainText[3]};
     char rightHalf[4]={plainText[4],plainText[5],plainText[6],plainText[7]};
@@ -56,21 +54,29 @@ void swap(char* a, char* b)
 
 void mangler (char* temp)
 {
-    bool expandedMatrix[8][6];
-    fillExpansionMatrix(temp);
+    int expandedMatrix[48];
+    fillExpansionMatrix(temp,expandedMatrix);
+    int key[48]={0,1,0,1,0,1,1,1,0,1,1,0,1,0,1,1,0,1,0,1,1,0,1,1,0,1,1,0,1,0,1,0,1,0,1,1,0,1,0,1,1,1,0,1,1,1,1,1};
+    
+    for(int i=0;i<48;i++){
+        expandedMatrix[i]=expandedMatrix[i] ^ key[i];
+    }
+
+    int reducedMatrix[4];
+    sbox(expandedMatrix,reducedMatrix);
+
+
+
 }
 
-void fillExpansionMatrix(char* x)
+void fillExpansionMatrix(char* x,int *y)
 {   
     int matrix[4][8];
-    // 1 2 3 4 5 6 7 8
-    // 9 10 11 12 13 14 15 16
-    // 17 18 19 20 21 22 23 24
-    // 25 26 27 28 29 30 31 32
     
     for(int i=0;i<4;i++){
         charToBinary(x[i],matrix[i]);
-    }  
+    }
+      
     int linearForm[32];
     
     
@@ -90,11 +96,13 @@ void fillExpansionMatrix(char* x)
 		28, 29, 28, 29, 30, 31, 32, 1};
     
     int expandedMatrix[6][8];
+    
     int k=0;
     for(int i=0;i<6;i++){
         for(int j=0;j<8;j++){
             expandedMatrix[i][j]=linearForm[expansionMatrix[k]-1];
-            printf("%d", expandedMatrix[i][j]);
+            x[i]=expandedMatrix[i][j];
+            // printf("%d", expandedMatrix[i][j]);
             k++;
         }
         printf("\n");
@@ -116,6 +124,79 @@ void charToBinary(int c, int* x)
         c=c/2;
         // printf(".%d", x[i]);
     }
-    printf("\n");
+    // printf("\n");
     
+}
+int binToInt(int x)
+{
+    int integer=0;
+    int k=0;
+    while(x>0){
+        integer=(x%2)*k;
+        k*=10;
+        x=x/2;
+    }
+    return integer;
+
+}
+void sbox(int* x, int* y){
+    int row[4], col[8];
+    
+    int start=0,end=5;
+    for(int i=0;i<8;i++){
+        row[i]= binToInt(x[start]*10 + x[end]);
+        start=end+1; end+=5;
+        col[i]=binToInt(x[start+1]+x[start+2]+x[start+3]+x[start+4]);
+    }
+
+    int s[8][4][16]= {{{14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7}
+,{0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8}
+,{4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0}
+,{15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13}
+}
+,{{15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10}
+,{3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5}
+,{0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15}
+,{13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9}
+}
+,{{10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8}
+,{13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1}
+,{13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7}
+,{1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12}
+}
+,{{7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15}
+,{13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9}
+,{10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4}
+,{3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14}
+}
+,{{2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9}
+,{14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6}
+,{4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14}
+,{11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3}
+}
+,{{12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11}
+,{10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8}
+,{9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6}
+,{4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13}
+}
+,{{4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1}
+,{13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6}
+,{1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2}
+,{6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12}
+}
+,{{13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7}
+,{1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2}
+,{7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8}
+,{2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11}
+}
+};
+
+
+    for(int i =0; i<8; i++){
+        y[i]=s[i][row[i]][col[i]];
+        printf("%d", y[i]);
+    }
+    
+
+
 }
