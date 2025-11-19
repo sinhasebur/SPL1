0a696a66db2a1f50498f15075282147b9a4520a2
Hasebur Sinha <sinhasebur932@gmail.com>
2025-11-20 02:05:57 +0600

    DESattempt

---
+++ PracticeCodes/generate.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string x;
+    cin>>x;
+    for(int i=0;i<16;i++){
+        if(x[i]=='0') x[i]='1';
+        else if(x[i]=='1') x[i]='0';
+        else
+        cout<<"{"<<x<<"}"<<endl; 
+       
+    }
+}
